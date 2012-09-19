dccdf52b05d0d5363b9052b599d7eb952f032bf4
MiCHiLU <djmchl@gmail.com>
2012-09-19 10:37:17 +0900

    Update README

--- README.txt
+++ README.txt
@@ -1,5 +1,6 @@
 This is a backport of the Python 3.2 functools module for use on
-Python versions 2.4 through 2.7.
+Python versions 2.7 and PyPy. It includes new features
+`lru_cache` (Least-recently-used cache decorator).
 
 Refer to the Python 3.2 documentation for usage information:
  http://docs.python.org/3.2/library/functools.html
--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from distutils.core import setup
 
 
 def main():
-    if not sys.version.startswith('2.'):
-        sys.stderr.write('This backport is for Python 2.x only.\n')
+    if not (3,) > sys.version_info >= (2, 7):
+        sys.stderr.write('This backport is for Python 2.7 only.\n')
         sys.exit(1)
 
     setup(
       name='functools32',
       version='3.2.3',
-      description='Backport of the functools module from Python 3.2.3 for use on 2.x.',
+      description='Backport of the functools module from Python 3.2.3 for use on 2.7 and PyPy.',
       long_description="""
 This is a backport of the functools standard library module from
-Python 3.2.3 for use on Python 2.4, 2.5, 2.6 and 2.7. It includes
+Python 3.2.3 for use on Python 2.7 and PyPy. It includes
 new features `lru_cache` (Least-recently-used cache decorator).""",
       license='PSF license',
 
